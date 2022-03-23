80d2034e8345a5ee141946b685892a7d95c9b3fc
taojianhang <1811783973@qq.com>
2022-03-23 16:53:32 +0800

    服务端构建测试

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState, useEffect, version } from "react";
-import { Button, Tag } from "antd";
+import { Button, Tag, message } from "antd";
 import styled from "styled-components";
 
 function App() {
+  const info = () => {
+    message.info("This is a normal message");
+  };
+
   return (
     <>
       <h1>Github Action Simple:{version}</h1>
       <Button type="primary">Antd测试</Button>
-      <Tag  color="orange">Antd测试</Tag>
+      <Tag color="orange">Antd测试</Tag>
+      <Button type="dashed" onClick={info}>
+        Display normal message
+      </Button>
+      ,
     </>
   );
 }
